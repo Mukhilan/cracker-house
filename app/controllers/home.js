936b68a2dffdce66c34ddd26e962839ff03a0894
mukhilan.jr <mukhilan.jr@zohocorp.com>
2018-09-23 14:57:12 +0530

    Add home footer

---
+++ app/controllers/home.js
@@ -0,0 +1,29 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+	bestSellingCrackers: [{
+		name: '5000 wala lar',
+		imageURL: 'assets/images/garlands/garland-4.jpg'
+	}, {
+		name: 'Challenger (Whistling)',
+		imageURL: 'assets/images/rockets/rocket-2.jpg'
+	}, {
+		name: 'Happy planet-anar',
+		imageURL: 'assets/images/flower-pots/planet-anar.gif'
+	},{
+		name: 'Colour Pinjore Deluxe',
+		imageURL: 'assets/images/flower-pots/color-pinjore.jpg'
+	}, {
+		name: '56 Gaint crackers',
+		imageURL: 'assets/images/chorsa/chorsa-3.jpg'
+	},{
+		name: 'Stripped bijili 100',
+		imageURL: 'assets/images/bijili/bijili-3.jpg',
+	}, {
+		name: '4 twinkling star',
+		imageURL: 'assets/images/twinkling/twinkling-2.jpg'
+	},{
+		name: 'Super bullet',
+		imageURL: 'assets/images/bombs/bomb-3.jpg'
+	}]
+});
